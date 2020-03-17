e10dbf69b31800a233f53e50fb4509dddc4fb986
iMath97 <camusmathias97@gmail.com>
2020-03-17 15:03:25 +0100

    added style

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
+    config.addPassthroughCopy("src/css");
 
-    return {        
+    return {   
+        passthroughFileCopy: true,     
         dir: {
             input: "src",
             output: "dist"
